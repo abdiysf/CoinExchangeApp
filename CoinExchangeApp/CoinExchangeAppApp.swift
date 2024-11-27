f691a198643103e98dc60c2adebdcfee8e6f2e56
abdiysf <mustafeabdisalam@gmail.com>
2024-11-28 00:56:30 +0300

    Initial Commit

---
+++ CoinExchangeApp/CoinExchangeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinExchangeAppApp.swift
+//  CoinExchangeApp
+//
+//  Created by Abdirahman Standard on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinExchangeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
